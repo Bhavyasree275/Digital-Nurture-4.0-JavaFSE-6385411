1e8215758d672cfe08d40c4be107650343734e1c
Bhavya <139208288+Bhavyasree275@users.noreply.github.com>
2025-07-01 21:44:47 +0530

    Add files via upload

---
+++ Spring Core and Maven/Exercise 4: Creating and Configuring a Maven Project/Book.java
@@ -0,0 +1,16 @@
+package com.library;
+public class Book {
+    private String title;
+
+    public Book() {
+        this.title = "Spring in Action";
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
