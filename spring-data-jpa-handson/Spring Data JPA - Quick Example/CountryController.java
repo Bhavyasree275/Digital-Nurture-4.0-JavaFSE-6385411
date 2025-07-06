c7772b034ff5d34f709e42bd2d7bdbf736aacd2c
Bhavya <139208288+Bhavyasree275@users.noreply.github.com>
2025-07-06 15:57:31 +0530

    Add files via upload

---
+++ spring-data-jpa-handson/Spring Data JPA - Quick Example/CountryController.java
@@ -0,0 +1,23 @@
+package com.cognizant.ormlearn.controller;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
