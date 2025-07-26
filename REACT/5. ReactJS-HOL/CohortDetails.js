54a0258c5d146817319bcb78ad8a76c792211122
Bhavya <139208288+Bhavyasree275@users.noreply.github.com>
2025-07-26 23:11:27 +0530

    Add files via upload

---
+++ REACT/5. ReactJS-HOL/CohortDetails.js
@@ -0,0 +1,27 @@
+// src/Components/CohortDetails.js
+import React from 'react';
+import styles from './CohortDetails.module.css';
+
+const CohortDetails = ({ cohort }) => {
+  const statusColor = cohort.status.toLowerCase() === "ongoing" ? "green" : "blue";
+
+  return (
+    <div className={styles.box}>
+      <h3 style={{ color: statusColor }}>
+        {cohort.id} - {cohort.track}
+      </h3>
+      <dl>
+        <dt>Started On</dt>
+        <dd>{cohort.startDate}</dd>
+        <dt>Current Status</dt>
+        <dd>{cohort.status}</dd>
+        <dt>Coach</dt>
+        <dd>{cohort.coach}</dd>
+        <dt>Trainer</dt>
+        <dd>{cohort.trainer}</dd>
+      </dl>
+    </div>
+  );
+};
+
+export default CohortDetails;
